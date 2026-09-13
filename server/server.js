6e5df8707033f9a6d89b651fe0e8a4265753975c
Shital-3 <shital.nirmal23@pcu.edu.in>
2026-09-13 18:31:34 +0530

    Fix server host for deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import app from "./src/app.js";
 
 const port = Number(process.env.PORT || 5000);
 
-const server = app.listen(port, () => {
+const server = app.listen(port, "0.0.0.0", () => {
   console.log(`Pathfinder API listening on port ${port}`);
 });
 
